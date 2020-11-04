0ef47dfe74fc8ebc34724b4f29253d912030a1c4
Margaux Mansanarez <margaux.mansanarez@gmail.com>
2020-11-04 14:30:32 +0100

    Yelp MVP: rake passed & style done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+
+puts 'Creating restaurants...'
+5.times do
+  restaurant = Restaurant.new(
+    name:         Faker::Restaurant.name,
+    address:      "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    phone_number: Faker::PhoneNumber.phone_number,
+    category:     CATEGORIES.sample
+  )
+  restaurant.save!
+end
+puts 'Finished!'
